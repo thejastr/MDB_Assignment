ac3e70be77c781f67493f2cd3bf9e647b366442e
thejastr <47317494+thejastr@users.noreply.github.com>
2021-05-28 16:07:28 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,30 @@
 import socket
+import os
+from _thread import *
 
-s = socket.socket()
-port = 3125
-s.connect(('localhost', port))
-z = 'Welcome to Manipal'
-s.sendall(z.encode())    
-s.close()
+ServerSideSocket = socket.socket()
+host = '127.0.0.1'
+port = 2004
+ThreadCount = 0
+try:
+    ServerSideSocket.bind((host, port))
+except socket.error as e:
+    print(str(e))
+
+print('Socket is listening..')
+ServerSideSocket.listen(5)
+
+def client(connection,address):
+    connection.send(str.encode('Server is working:'))
+    while True:
+        data = connection.recv(2048)
+        print("Msg from",address," : ",data.decode('utf-8'))
+        if data.decode('utf-8')=="bye":
+            break
+    connection.close()
+
+while True:
+    Client, address = ServerSideSocket.accept()
+    print('Connected to: ' + address[0] + ':' + str(address[1]))
+    start_new_thread(client, (Client,address ))
+ServerSideSocket.close()
